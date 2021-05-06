a409a80e93d60338b42ae5d75e3e74cbe12b8ee9
Larivelsa <larissavelsa@gmail.com>
2021-05-06 13:15:49 -0300

    basico2

---
+++ basico_1/arrays.py
@@ -0,0 +1,8 @@
+
+parte_1 = 'Larôss'
+parte_2 = "a"
+
+nome = parte_1 + parte_2
+
+print(nome)
+print(id(nome))
---
+++ basico_1/primeiroProjeto/main.py
@@ -0,0 +1,13 @@
+nota1 = 9
+nota2 = 1
+nota3 = 7
+
+media = (nota1+nota2+nota3)/3;
+
+if(media>=7):
+    print('Aprovada!')
+    print (media)
+else:
+    print('Reprovada!')
+    print('Boa sorte na recuperacao')
+    print(media)
