cef1c1173b3fc8972ef66cf08ec755617c7629ee
Larivelsa <larissavelsa@gmail.com>
2021-05-08 09:22:42 -0300

    iniciando projetos

---
+++ basico_2/projetos/area_circunferencia_v1.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+# o python 3 já usa esta codificação, mas caso seja necessário, tá aí
+# formula da circunferencia: area = pi * r²
+
+
+pi = 3.1415
+raio = 10
+area = pi * raio ** 2
+print(f'A área da circunferência é de {area}')
+
