585469ba5dd902a9fcc195aea9c505baa3992b79
Larivelsa <larissavelsa@gmail.com>
2021-05-09 14:22:54 -0300

    REP

---
+++ basico_2/estruturas_controle/set.py
@@ -0,0 +1,16 @@
+PALAVRAS_PROIBIDAS = {'futebol','religião','política'}
+
+textos = [
+    'João gosta de futebol e política',
+    'A praia foi divertida',
+    'futebol'
+]
+
+print(textos in PALAVRAS_PROIBIDAS)
+
+
+
+
+
+
+            
