66fe81d5ed0fc6b1a6d72987177ce6fceae4a924
Larivelsa <larissavelsa@gmail.com>
2021-05-26 09:20:00 -0300

    Delete basico_2 directory

--- basico_2/inicial/area_circulo_v2.py
+++
@@ -1,6 +0,0 @@
-from math import pi
-#print(dir()) dir() exibe as variaveis que estão no escopo
-raio = input('Informe o raio: ')
-area = pi * float(raio) ** 2
-print(f'A área da circunferência é de {area}')
-
