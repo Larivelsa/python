341b8fc2b6a47d2459916068b58a4314f258e76f
larissa <larissavelsa@gmail.com>
2021-05-25 22:18:35 -0300

    mega

--- alura/jogos/megasena.py
+++ alura/jogos/megasena.py
@@ -1,1 +1,19 @@
 ﻿# gerar conjunto de jogos
+import random
+fixos = (12, 53, 56, 33)
+
+
+numero_jogos = 3
+while numero_jogos > 0:
+
+    jogo = list(fixos)
+    jogo.append(random.randrange(1, 61))
+    jogo.append(random.randrange(1, 61))
+    jogo.sort()
+    print(jogo)
+    print('------------------------')
+
+    numero_jogos -= 1
+
+
+# fix: problema com números repetidos
