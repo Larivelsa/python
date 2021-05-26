0b1258e82e9807d11a8aca6b384432762db496dd
larissa <larissavelsa@gmail.com>
2021-05-25 21:55:47 -0300

    iniciando OO

---
+++ alura/orientacao_objetos/conta.py
@@ -0,0 +1,23 @@
+﻿class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto...")
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print(f"Saldo {self.__saldo} do titular {self.titular}")
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+
+conta = Conta(12, 'larissa', 500, 8000)
