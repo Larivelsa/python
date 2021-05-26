28a85bd3fbe39aa06f69b2549aac66f4ac8b11f0
Larivelsa <larissavelsa@gmail.com>
2021-05-26 09:20:23 -0300

    Delete manipulacao_arquivos directory

--- manipulacao_arquivos/io_v6.py
+++
@@ -1,5 +0,0 @@
-﻿import csv
-
-with open('C:/Users/Larissa/github/python/manipulacao_arquivos/pessoas.csv') as entrada:
-    for pessoa in csv.reader(entrada):
-        print('Nome: {} Idade: {}'.format(*pessoa))
