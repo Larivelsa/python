df52e3eff046542f45ee4ac3867ba2cac85d8665
Larivelsa <larissavelsa@gmail.com>
2021-05-29 16:38:17 -0300

    finalização do curso de avançando com oo

--- alura/orientacao_objetos_parte_2/filme.py
+++ alura/orientacao_objetos_parte_2/filme.py
@@ -2,6 +2,13 @@
 # sem o construtor nós precisaríamos definir os atributos depois de instanciar,
 # teríamos o trabalho de descobrir quais atributos definir.
 
+from collections.abc import MutableSequence
+
+
+class MinhaListinhaMutavel(MutableSequence):
+    pass
+
+
 class Programa:
     def __init__(self, nome, ano):
         self._nome = nome.title()
@@ -25,6 +32,9 @@ class Programa:
 
     def __str__(self):
         return f'{self.nome} - {self.ano} - {self.likes} likes'
+
+    def __getitem__(self, item):
+    return self._programas[item]
 
 
 class Filme(Programa):
@@ -59,6 +69,18 @@ class Playlist():
         return len(self._programas)
 
 
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item)
+    return self._programas[item]
+
+    def __len__(self):
+        return len(self._programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
 tmep = Filme('todo mundo em pânico', 1999, 100)
@@ -76,8 +98,8 @@ demolidor.dar_likes()
 
 filmes_e_series = [vingadores, atlanta, tmep, demolidor]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
-print(f'Tamanho da playlist: {len(playlist_fim_de_semana.listagem)}')
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
 
 # python é lindo
-for programa in playlist_fim_de_semana.listagem:
+for programa in playlist_fim_de_semana:
     print(programa)
