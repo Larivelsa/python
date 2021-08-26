b287c508faf2ede2b5ece454e5dbc5640817a7ed
larissa <larissavelsa@gmail.com>
2021-08-26 14:03:30 -0300

    .

--- alura/flask/jogatina.py
+++ alura/flask/jogatina.py
@@ -3,13 +3,14 @@ instalar o flask na versão abaixo:
 pip3 install flask==0.12.2
 '''
 
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__) #__name__ obtem nome do módulo#
+app = Flask(__name__)  # __name__ obtem nome do módulo#
+
 
 @app.route('/inicio')
 def ola():
-    return '<h1>Hey you</h1>'
+    return render_template('lista.html', titulo='Jogos')
+
 
 app.run()
-
