dc0432b5b3fecb65f54f2a7e007ae5f6830e8661
Larissa <larissavelsa@gmail.com>
2021-08-27 08:27:42 -0300

    .

--- alura/flask/jogatina.py
+++ alura/flask/jogatina.py
@@ -7,10 +7,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)  # __name__ obtem nome do módulo#
 
+class Jogo:
+    def __init__(self,nome,categoria,console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 
 @app.route('/inicio')
-def ola():
-    return render_template('lista.html', titulo='Jogos')
 
+def ola():
+    jogo1 = Jogo('Super Mario','Arcade','Nintendo')
+    jogo2 = Jogo('Pokemon Gold','RPG','GBA')
+    jogo3 = Jogo('Mortal Kombat','Luta','Nintendo')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
