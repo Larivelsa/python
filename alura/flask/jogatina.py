fb18a160c51fd25971a82c7e9ea64f80373507d1
larissa <larissavelsa@gmail.com>
2021-08-27 13:19:14 -0300

    iniciando com boots

--- alura/flask/jogatina.py
+++ alura/flask/jogatina.py
@@ -7,16 +7,19 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)  # __name__ obtem nome do módulo#
 
+
 class Jogo:
-    def __init__(self,nome,categoria,console):
+    def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
 
-jogo1 = Jogo('Super Mario','Arcade','Nintendo')
-jogo2 = Jogo('Pokemon Gold','RPG','GBA')
-jogo3 = Jogo('Mortal Kombat','Luta','Nintendo')
+
+jogo1 = Jogo('Super Mario', 'Arcade', 'Nintendo')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'Nintendo')
 lista = [jogo1, jogo2, jogo3]
+
 
 @app.route('/')
 def index():
@@ -27,13 +30,15 @@ def index():
 def novo():
     return render_template('novo.html', titulo='Novo jogo')
 
-@app.route('/criar', methods=['post',])
+
+@app.route('/criar', methods=['post', ])
 def criar():
-    nome = request.form['nome'] 
-    categoria = request.form['categoria'] 
+    nome = request.form['nome']
+    categoria = request.form['categoria']
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
     return redirect('/')
+
 
 app.run(debug=True)
