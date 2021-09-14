bb4759b04da289b23d5edb694ddbf60b1e515c7c
Larissa <larissavelsa@gmail.com>
2021-09-14 12:49:36 -0300

    iniciando corretamente sobre mysql-python

---
+++ bd_mysql/conectar_server_db.py
@@ -0,0 +1,24 @@
+# Primeiro, é necessário importar a API de comunicação entre o MySQL e Python
+import mysql.connector
+
+# Caso o driver da API não esteja instalado, fazer o download usando o pip:
+# por exemplo, em C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>python -m pip install mysql-connector-python
+
+# Agora vamos instanciar um objeto contendo as informações de conexão, usando a função connect,
+# retornando um objeto MySQLConnection ou PooledMySQLConnection.
+# Na função connect, passamos argumentos nomeados.
+# Para conectar com server, passamos apenas user, password e host.
+# Se necessário conectar ao db, informa database nos parâmetros.
+
+# Exemplo de conexão com o server, sem expecificar a base de dados:
+conexao_server = mysql.connector.connect(user='root',
+                                        password='root',
+                                        host='localhost')
+
+# Exemplo de conexão com o server e também com um db:
+conexao_server_e_bd = mysql.connector.connect(user='root',
+                                              password='root',
+                                              host='localhost',
+                                              database='teste')
+
+                                              
---
+++ bd_mysql/criar_db.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+conexao_server = mysql.connector.connect(user='root',
+                                         password='root',
+                                         host='localhost')
+
+# A classe cursor possibilita que o Python manipule sentenças SQL.
+# Os cursores são criados pelo método connection.cursor() e isso faz
+# com que os cursores fiquem vinculados à conexão e os comandos
+# são executados no contexto da sessão de db envolvida na conexão.
+cursor = conexao_server.cursor()
+
+# Para ficar com o código mais legível, criei uma variável para
+# receber a string com a setença SQL.
+sql = 'CREATE DATABASE teste'
+
+# Passamos como argumento a sentença sql para o método .execute do objeto cursor
+cursor.execute(sql)
+
+# E pronto, o banco de dados foi criado via Python!
+
+# E agora, fechamos a conexão após usar o cursor.
+# Isso garante a redefinição de todo seu conteúdo e 
+# remoção de referências ao objeto criado anteriormente.
+cursor.close()
+
+'''
+explicação pq não usamos aqui o commit:
+
+Este método envia uma COMMITinstrução ao servidor MySQL, confirmando a transação atual. 
+Como, por padrão, o Conector / Python não se autocometa, é importante chamar esse método 
+após cada transação que modifica dados para tabelas que usam mecanismos de armazenamento transacional.
+
+'''
