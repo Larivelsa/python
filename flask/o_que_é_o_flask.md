d955fa9848a1daa8dc58aa6ba9b2476e26449b7a
larissa <larissavelsa@gmail.com>
2021-09-23 20:30:25 -0300

    mais sobre o inícios

---
+++ flask/o_que_é_o_flask.md
@@ -0,0 +1,5 @@
+### O que é o o Flask
+- É um microfirmware (micro porque há apenas duas dependências instaladas) de Python para web
+- As duas dependências são: Jinja (templates) e Werkzeug (ferramenta para server web)
+- Jinja é um mecanismo de template em que usa-se marcadores parecidos com a sintaxe de Python e depois o template recebe dados para renderizar a página final. (fonte: https://jinja.palletsprojects.com/)
+- WSGI é um acrônimo para Web Server Gateway Interface, trata-se de uma forma para servidores conversarem com frameworks, e vice-versa (fonte: http://devfuria.com.br/linux/instalando-apache-wsgi/)
--- flask/thinking_money/thinking_money.py
+++ flask/thinking_money/thinking_money.py
@@ -1,16 +1,16 @@
 from flask import Flask
-# fazer pesquisa aqui: https://flask.palletsprojects.com/en/2.0.x/quickstart/
+# a classe Flask importada é para construção do aplicativo WSGI
 
-# __name__ é o nome do módulo
-# por exemplo, se estiver trabalhando no script atual
-# o __name__ será main, mas se importamos um script
-# chamado fibo no script main, o __name__
-# será fibo
+# criando instância da classe e passando como parâmetro o nome do módulo onde o Flask vai procurar os recursos (modelos e arquivos estáticos)
 app = Flask(__name__)
 
 # cria uma rota para server/inicio
 # é necessário que haja uma função abaixo para gerar
 # o conteúdo da página
+
+# @app.route faz parte do Flask e é um decorator e# um decorator é um método
+# que envolve uma função e modifica seu comportamento (ainda não entendi completamente...)
+# e diz qual URL deve ser acionar a função
 
 
 @app.route('/inicio')
@@ -18,4 +18,6 @@ def ola():
     return '<h2>Olá Flask!</h2>'
 
 
+# usa-se degub=True como parâmetro nomeado para que seja possível a visualização do log de
+# acesso às páginas e também possibilitar o seu reinício em caso de alteração no código-fonte
 app.run(debug=True)
