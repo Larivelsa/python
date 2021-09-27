ebec0826aaefd28b82f2e58f27256076ca2d1e55
larissa <larissavelsa@gmail.com>
2021-09-27 13:34:28 -0300

    criando objeto e página html

--- flask/thinking_money/thinking_money.py
+++ flask/thinking_money/thinking_money.py
@@ -1,21 +1,54 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 # a classe Flask importada é para construção do aplicativo WSGI
 
 # criando instância da classe e passando como parâmetro o nome do módulo onde o Flask vai procurar os recursos (modelos e arquivos estáticos)
 app = Flask(__name__)
 
+
+class Receita:
+    def __init__(self, nome, tipo, data, valor):
+        self.nome = nome
+        self.tipo = tipo
+        self.data = data
+        self.valor = valor
+
+
 # cria uma rota para server/inicio
 # é necessário que haja uma função abaixo para gerar
 # o conteúdo da página
 
-# @app.route faz parte do Flask e é um decorator e# um decorator é um método
-# que envolve uma função e modifica seu comportamento (ainda não entendi completamente...)
-# e diz qual URL deve ser acionar a função
+# @app.route faz parte do Flask diz qual URL deve ser acionar a função
+# e é decorator, sendo isso um padrão de projeto que adiciona
+# uma nova funcionalidade a um objeto já existente
+# fonte: https://www.datacamp.com/
 
 
 @app.route('/inicio')
 def ola():
-    return '<h2>Olá Flask!</h2>'
+    return f'<h2>Olá thinker money!</h2>'
+
+
+@app.route('/receita')
+def nova():
+    return render_template('receita.html')
+
+
+@app.route('/receita_criar', methods=['POST', ])
+def receita_criar():
+    nome = request.form['nome']
+    tipo = request.form['tipo']
+    data = request.form['data']
+    valor = request.form['valor']
+
+    receita = Receita(nome, tipo, data, valor)
+    lista = lista.append(receita)
+
+    return lista
+
+
+@app.route('/e')
+def e():
+    return receita_criar().lista
 
 
 # usa-se degub=True como parâmetro nomeado para que seja possível a visualização do log de
@@ -23,3 +56,5 @@ def ola():
 # é usado apenas em ambiente de desenvolvimento, para produção usa-se a o deploy do WSGI
 # e que não faz parte do objeto instanciado da classe
 app.run(debug=True)
+
+# próximo passo: ver templates
