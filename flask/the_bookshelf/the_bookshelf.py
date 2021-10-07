a0a6647318163fb932f38a932b1e68dc40c84294
larissa <larissavelsa@gmail.com>
2021-10-07 20:47:04 -0300

    trabalhando com conteúdo dinâmico (server)

--- flask/the_bookshelf/the_bookshelf.py
+++ flask/the_bookshelf/the_bookshelf.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 # http://devfuria.com.br/python/modulos-pacotes/
 # http://devfuria.com.br/python/entenda-__name__-__main__/
 
-@app.route('/')
-def barra():
-    return '<h1>É barra</h1>'
+
+@app.route('/inicial')
+def barras():
+    return render_template('inicial.html')
+
+
+@app.route('/list')
+def listar():
+    lista = ('O Cavaleiro Preso na Armadura',
+             'A Riqueza de uma Vida Simples', 'Os Segredos da Mente Milionária')
+    return render_template('lista.html', livros=lista)
+
 
 app.run(debug=True)
