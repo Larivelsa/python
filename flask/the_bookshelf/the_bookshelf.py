e12c1f61713746f907cbf0d64d3cc578c7ae416b
larissa <larissavelsa@gmail.com>
2021-10-21 21:12:53 -0300

    mudanças no css

--- flask/the_bookshelf/the_bookshelf.py
+++ flask/the_bookshelf/the_bookshelf.py
@@ -1,34 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 # http://devfuria.com.br/python/modulos-pacotes/
 # http://devfuria.com.br/python/entenda-__name__-__main__/
 
+
 class Livro:
-    def __init__(self,titulo, autor, genero, sinopse):
+    def __init__(self, titulo, autor, genero, sinopse):
         self.titulo = titulo
         self.autor = autor
         self.genero = genero
         self.sinopse = sinopse
 
+
 lista = []
+
 
 @app.route('/inicial')
 def barras():
     return render_template('inicial.html')
 
+
 @app.route('/novo')
 def novo():
     return render_template('novo.html')
 
-@app.route('/inserir')
+
+@app.route('/inserir', methods=['POST', ])
 def inserir():
-    l1 = Livro('Os Segredos da Mente Milionária','T. Harv Eker','Finanças','Possuir mindset de riqueza é essencial.')
-    lista.append(l1)
-    return render_template('listar.html', livros=lista)
+    titulo = request.form['titulo']
+    autor = request.form['autor']
+    genero = request.form['genero']
+    sinopse = request.form['sinopse']
+    livro = Livro(titulo, autor, genero, sinopse)
+    lista.append(livro)
+
+    if lista:
+        mensagem = 'Livro inserido com sucesso!'
+    else:
+        mensagem = 'O livro não foi inserido.'
+
+    return render_template('novo.html', mensagem=mensagem)
+
 
 @app.route('/listar')
 def listar():
-    return render_template('listar.html',livros=lista)
+    return render_template('listar.html', livros=lista)
 
-app.run(debug=True) 
+
+app.run(debug=True)
