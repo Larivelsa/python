d555ddfac5b05ed1c5ed606178eb912028bf8e5f
larissavelsa@gmail.com <larissavelsa@gmail.com>
2025-05-10 17:22:31 -0300

    Adiciona implementação inicial do Algoritmo de Luhn com comentários explicativos

---
+++ freeCodeCamp/02-Luhn-algorithm.py
@@ -0,0 +1,21 @@
+# O Algoritmo de Luhn é muito usado 
+# para verificar se certos números, 
+# como os de cartões de crédito, estão corretos 
+# ou não (verificação de erros).
+# Ao desenvolver esse projeto, vou praticar cálculos 
+# numéricos e manipulação de strings, o que vai me 
+# ajudar a entender melhor como funcionam validações 
+# e como tratar informações em formato de texto ou número.
+
+
+
+
+
+
+
+
+
+
+
+
+
