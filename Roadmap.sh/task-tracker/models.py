2c7c6dd5b61b133b9df954ff4fc126a68fb9ec28
Laris <larissavelsa@gmail.com>
2025-12-16 19:44:42 -0300

    Delete Roadmap.sh/task-tracker directory

--- Roadmap.sh/task-tracker/models.py
+++
@@ -1,8 +0,0 @@
-class Task:
-    def __init__(self,id,description,status,created_at,updated_at):
-        self.id = id
-        self.description = description
-        self.status = status
-        self.created_at = created_at
-        self.updated_at = updated_at
-
